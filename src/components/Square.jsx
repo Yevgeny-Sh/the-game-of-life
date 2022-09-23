6f6233669b446d2c019754517955a14571ae0553
Yevgeny-Sh <yevgst7@gmail.com>
2022-09-24 01:24:34 +0300

    start makin components.

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Square(props) {
-  return <div className="square"></div>;
+  return (
+    <div className="square">
+      <button
+        className={`btn  ${props.className}`}
+        onClick={() => props.setAlive(props.x, props.y)}
+        // {...props.className.toString()} why this line causes warning?
+      />
+    </div>
+  );
 }
