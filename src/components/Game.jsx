683e9e1fe7874b66ec2f256a8fb00ab57b74658b
Yevgeny-Sh <yevgst7@gmail.com>
2022-09-24 05:27:57 +0300

    start makin components........

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function Game() {
-  return <div>Game</div>;
+  const boardRef = useRef(null);
+
+  const [gridSize, setGridSize] = useState(10);
+  const [seed, setSeed] = useState(4);
+  const [board, setBoard] = useState(
+    Array(gridSize)
+      .fill()
+      .map(() => Array(gridSize).fill(false))
+  );
+  const [generetion, setGeneretion] = useState(0);
+
+  return (
+    <div className="game-div" ref={boardRef}>
+      <h1>game</h1>
+      <h1>generetion:{generetion} </h1>
+
+      <button className="btn play-btn" onClick={() => seedArr(board)}>
+        seed
+      </button>
+      <button className="btn seed-btn" onClick={() => handlePlay()}>
+        play
+      </button>
+      <Board board={board} size={gridSize} setAlive={setAlive}></Board>
+    </div>
+  );
 }
