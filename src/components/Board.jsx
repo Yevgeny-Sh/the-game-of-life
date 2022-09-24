59f5284bc5b9fb0a48fd81f939925af5ec09864f
Yevgeny-Sh <yevgst7@gmail.com>
2022-09-24 04:57:20 +0300

    start makin components....

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 
 export default function Board() {
+  let sqaures = props.board.map((col, i) => {
+    return col.map((sqr, j) => {
+      return (
+        <Square
+          x={i}
+          y={j}
+          live={isAlive(i, j).toString()}
+          setAlive={props.setAlive}
+          className={isAlive(i, j) === true ? "green" : "red"}
+          key={` ${i}+${j}`}
+        >
+          {` ${props.size}`}
+        </Square>
+      );
+    });
+  });
+
   return (
     <div className="board-container">
       <div className="board">{/* {sqaures} */}</div>
